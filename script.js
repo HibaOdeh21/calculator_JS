47bf4d4180f7931344918608909e9bdf694a2df4
HibaOdeh21 <hibaodeh15@gmail.com>
2023-09-20 20:24:32 +0300

    calculation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-class calculator{
+class Calculator{
     constructor(previousOperandTextElem,currentOperandTextElem){
         this.previousOperandTextElem=previousOperandTextElem
         this.currentOperandTextElem=currentOperandTextElem
@@ -12,29 +12,60 @@ class calculator{
     }
 
     delete(){
+        this.currentOperand = this.currentOperand.toString().slice(0,-1)
 
     }
 
     appendNumber(number){
+        if (number === '.'&& this.currentOperand.includes('.'))  return 
+        this.currentOperand= this.currentOperand.toString() + number.toString()
 
     }
-    chooseOperatin(operation){
+    chooseOperation(operation){
+        if (this.currentOperand==='') return
+        if(this.previousOperand!== ''){
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
 
     }
     compute(){
+        let computaion
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if(isNaN(prev)|| isNaN(current)) return
+        switch(this.operation){
+            case '+':
+                computaion = prev + current
+                break
+            case '-':
+                computaion = prev - current
+                break
+            case '*':
+                computaion = prev * current
+                break
+            case '%':
+                computaion = prev / current
+                break
+            default:
+                return                
+        }
+     this.currentOperand = computaion
+     this.operation =undefined
+     this.previousOperand =''
 
     }
     updateDisplay(){
+        this.currentOperandTextElem.innerText =this.currentOperand
+        this.previousOperandTextElem.innerText =this.previousOperand
 
     }
 
 
 
 }
-
-
-
-
 
 
 const numberButtons = document.querySelectorAll('[data-number]')
@@ -46,3 +77,33 @@ const previousOperandTextElem = document.querySelector('[data-previous-opperand]
 const currentOperandTextElem = document.querySelector('[data-current-opperand]')
 
 
+const calculator = new Calculator(previousOperandTextElem,currentOperandTextElem)
+
+numberButtons.forEach(button =>{
+    button.addEventListener("click",()=>{
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+operationButtons.forEach(button =>{
+    button.addEventListener("click",()=>{
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener("click",button =>{
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener("click",button =>{
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener("click",button =>{
+    calculator.delete()
+    calculator.updateDisplay()
+})
